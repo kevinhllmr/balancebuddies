087da0f9649c8a9d84c97daf008aca0e6b1a1d05
kevinhllmr <kevin.hollmeier@freenet.de>
2025-03-30 21:05:08 +0200

    MainMenu buttons changed

--- app/src/main/java/com/kevin/balancebuddies/MainMenuActivity.kt
+++ app/src/main/java/com/kevin/balancebuddies/MainMenuActivity.kt
@@ -13,11 +13,19 @@ class MainMenuActivity : Activity() {
         setContentView(R.layout.activity_main_menu)
 
         val singlePlayerButton: Button = findViewById(R.id.button_singleplayer)
-        val joinAsSecondPlayer: Button = findViewById(R.id.button_multiplayer)
+        val joinAsFirstPlayer: Button = findViewById(R.id.button_multiplayer1)
+        val joinAsSecondPlayer: Button = findViewById(R.id.button_multiplayer2)
 
         singlePlayerButton.setOnClickListener {
             val intent = Intent(this, GameActivity::class.java)
             intent.putExtra("MODE", "singleplayer")
+            intent.putExtra("IS_HOST", false)
+            startActivity(intent)
+        }
+
+        joinAsFirstPlayer.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            intent.putExtra("MODE", "multiplayer")
             intent.putExtra("IS_HOST", true)
             startActivity(intent)
         }
